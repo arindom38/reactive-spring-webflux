e5068aeb5b002878c09796297e8484319f5b2ff3
ujjal38 <kundu.vivasoft@bkash.com>
2023-03-10 11:07:52 +0600

    Added Flux Example 1

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,18 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux(){
+        return Flux.fromIterable(List.of("Aria", "rock","Nola"));
+    }
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+        service.namesFlux()
+                .subscribe(name-> System.out.println("Name is: "+name));
+    }
 }
