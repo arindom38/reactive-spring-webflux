1b32c4d3d152e266d8a2d6c6864ba7d10283c041
arindom38 <kundu.vivasoft@bkash.com>
2023-03-10 11:18:59 +0600

    Added Mono Example 1

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -9,10 +10,17 @@ public class FluxAndMonoGeneratorService {
     public Flux<String> namesFlux(){
         return Flux.fromIterable(List.of("Aria", "rock","Nola"));
     }
+
+    public Mono<String> nameMono(){
+        return Mono.just("Dilip");
+    }
     public static void main(String[] args) {
         FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
 
         service.namesFlux()
                 .subscribe(name-> System.out.println("Name is: "+name));
+
+        service.nameMono()
+                .subscribe(name-> System.out.println("Mono Name is: "+name));
     }
 }
