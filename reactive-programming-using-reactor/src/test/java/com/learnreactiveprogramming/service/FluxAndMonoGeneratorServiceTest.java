34ab7f886ff812149379e7a84af678f36dd55466
arindom38 <kundu.vivasoft@bkash.com>
2023-03-10 13:10:27 +0600

    Added text case for nameflux

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,23 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService serviceTest = new FluxAndMonoGeneratorService();
+
+    @Test
+    void nameFlux() {
+
+        var namesFlux = serviceTest.namesFlux();
+
+        StepVerifier.create(namesFlux)
+//                .expectNext("Aria", "rock","Nola")
+//                .expectNextCount(3)
+                .expectNextCount(2)
+                .expectNext("Rock")
+                .verifyComplete();
+    }
 }
