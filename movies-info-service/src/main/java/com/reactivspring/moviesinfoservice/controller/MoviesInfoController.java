da5e49094a2305656a3934bfe8df1b4a2639eabe
ujjal38 <kundu.vivasoft@bkash.com>
2023-03-19 16:34:26 +0600

    added movie info service and controller

---
+++ movies-info-service/src/main/java/com/reactivspring/moviesinfoservice/controller/MoviesInfoController.java
@@ -0,0 +1,22 @@
+package com.reactivspring.moviesinfoservice.controller;
+
+import com.reactivspring.moviesinfoservice.domain.MovieInfo;
+import com.reactivspring.moviesinfoservice.service.MoviesInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class MoviesInfoController {
+
+    private final MoviesInfoService moviesInfoService;
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo){
+        return moviesInfoService.addMovieInfo(movieInfo);
+    }
+}
---
+++ movies-info-service/src/main/java/com/reactivspring/moviesinfoservice/service/MoviesInfoService.java
@@ -0,0 +1,17 @@
+package com.reactivspring.moviesinfoservice.service;
+
+import com.reactivspring.moviesinfoservice.domain.MovieInfo;
+import com.reactivspring.moviesinfoservice.repository.MovieInfoRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class MoviesInfoService {
+
+    private final MovieInfoRepo movieInfoRepo;
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+       return movieInfoRepo.save(movieInfo).log();
+    }
+}
